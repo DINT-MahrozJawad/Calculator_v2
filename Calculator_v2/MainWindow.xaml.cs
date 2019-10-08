e0815638c0c2a269969805f9fe08dc444d3121d4
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-10-08 12:55:23 +0200

    Agregar archivos de proyecto.

---
+++ Calculator_v2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_v2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           // int contador = 1;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                
+                for (int j = 0; j < 3; j++)
+                {
+                    Viewbox vb = new Viewbox();
+                    Button button = new Button();
+                    TextBlock tb = new TextBlock();
+
+                    contenedor.Children.Add(button);
+                    button.Content = vb;
+                    vb.Child = tb;
+
+                    button.Tag = contador;
+
+                    tb.Text = $"{((i - 1) * 3) + j + 1}";
+                    button.Margin = new Thickness(10);
+                    button.Click += Button_Click;
+
+                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, j);
+
+                    
+                   // contador++;
+                }
+
+            }
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Texto.Text = Texto.Text + ((Button)sender).Tag.ToString();
+        }
+    }
+}
